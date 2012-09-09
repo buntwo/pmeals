14787ee6fac8545a160a4fd56168b22c48a6f73b
Brian Tu <briantu92@gmail.com>
2012-09-09 18:44:53 -0400

    Removed Toast import in VBM (accidentally included)

--- src/com/buntwo/pmeals/ViewByMeal.java
+++ src/com/buntwo/pmeals/ViewByMeal.java
@@ -42,7 +42,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.buntwo.pmeals.DatePickerDialogFragment.OnDateSelectedListener;
 import com.buntwo.pmeals.MealPickerDialogFragment.OnMealSelectedListener;
